fa4e0826ca44f1b9e1333bbe2c2bc7b658319d24
travon martin <martitra000@warren.k12.in.us>
2024-09-18 08:45:48 -0400

    ef

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame, random
 
 # Set display surface
 pygame.init()
@@ -9,68 +9,88 @@ WINDOW_HEIGHT = 400
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("Feed The Dragon")
 
-# Set FPS and clock
+#Set FPS and clock
 FPS = 60
 clock = pygame.time.Clock()
 
-# Set game values
-''' create a constant that stores player starting lives and give it a starting value of 5 '''  # PLAYER_STARTING_LIVES
-''' create a constant that stores player velocity and give it a starting value of 10 '''
-''' create a constant that stores coin starting velocity and give it a starting value of 10 '''
-''' create a constant that stores coin starting velocity and give it a starting value of 10 '''
-''' create a constant that stores coin acceleration and give it a starting value of 0.5 '''
+#Set game value:  CONSTANT_NAME, value
+PLAYER_STARTING_LIVES = 5
+PLAYER_VELOCITY = 10
+COIN_STARTING_VELOCITY = 10
+COIN_ACCELERATION = 0.5
+BUFFER_DISTANCE = 100
+#YOU do the remaining 4 CONSTANTS
 
-''' create a variable that tracks the score and give it a starting value of 0 '''
-''' create a variable that tracks the player lives and set it equal to the constant that stores player starting lives '''
-''' create a variable that tracks the coin velocity and set it equal to the constant that coin starting velocity '''
+#Set Game Variables:  variable_name
+' 3 variables'
+player_lives = PLAYER_STARTING_LIVES
+coin_velocity = COIN_STARTING_VELOCITY
+score = 0
+#YOU do the remaining 2 variables
 
-# Set colors
+#Set colors
 GREEN = (0, 255, 0)
-DARK_GREEN = (10, 50, 10)
+DARKGREEN = (10, 50, 10)
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 
-# Set fonts
-''' create a variable called font and give it a value from the following:  pygame.font.Font('AttackGraffiti.ttf', 32) '''
-Font = pygame.font.Font('AttackGraffiti.ttf', 32)
-# Set text
-# Sets the text for the score
-''' create a variable thate tracks score text and give it a value from the following:  font.render("Score: " + str(score), True, GREEN, DARK_GREEN) '''
-''' create a variable that tracks score rect and give it a value from the following:  score_text.get_rect() '''
-''' score_rect.topleft = (10, 10) '''
+#Set fonts
+font = pygame.font.Font('AttackGraffiti.ttf', 32)
 
-# Set the text for the title
-''' Same deal as score,  create a variable,  render the font with the following phrase: "Feed the Dragon", use True, and GREEN, WHITE '''
-''' Same deal as score, create a variable that tracks the rect of the title '''
-''' title_rect.centerx = WINDOW_WIDTH / 2'''
-''' title_rect.y = 10 '''
+#Set Text for Score
 
-# Sets the text for lives
-''' Same deal, variable name lives_text, "Lives: str(player_lives), True, GREEN, DARKGREEN '''
-''' Same deal, variable name lives_rect, get from lives_text '''
-''' Same deal, topright = (WINDOW_WIDTH - 10, 10)'''
 
-# Sets the text for game over
-'''
-Variable:  game_over_text
-Rect: game_over_rect
-PHRASE:  "GAMEOVER", 
-Antialias: True
-Color: GREEN, 
-Background: DARKGREEN,
-Position: center = (WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2),
-'''
+score_text = font.render("Score: " + str(score), True, GREEN, DARKGREEN)
+score_rect = score_text.get_rect()
+score_rect.topleft = (10, 10)
 
-# Sets the text for continue
+#Set Text for Title (Similar to Score)
+
+title_text = font.render("FEED THY DRAGON", True, GREEN, DARKGREEN)
+title_rect = title_text.get_rect()
+title_rect.centerx = WINDOW_WIDTH // 2
+title_rect.y = 10
+
+
+lives_text = font.render("Lives: " + str(player_lives), True, GREEN, DARKGREEN)
+lives_rect = score_text.get_rect()
+lives_rect.topright= (980, 10)
+#Set Text for Game Over (Similar to Score)
+
+center = (WINDOW_WIDTH//2, WINDOW_HEIGHT//2)
+game_over_text = font.render("ez your horrid", True, GREEN, DARKGREEN)
+game_over_rect = center
+
+#Set Text for Continue (Similar to Score)
 '''
-Variable:  continue_text
-Rect: continue_rect
-PHRASE:  "Press any key to play again", 
-Antialias: True
-Color: GREEN, 
-Background: DARKGREEN,
-Position: center = (WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2 + 32),
+variable names:  continue_text, continue_rect  
+render text: "Press any key to play again"
+antialias: True
+color: GREEN
+background: DARKGREEN
+rect location: center = (WINDOW_WIDTH//2, WINDOW_HEIGHT//2 + 32)
 '''
+coin_sound = pygame.mixer.Sound("coin_sound.wav")
+coin_sound.set_volume(0.2)
+miss_sound = pygame.mixer.Sound("miss_sound.wav")
+miss_sound.set_volume(0.5)
+pygame.mixer.music.load("ftd_background_music.wav")
+
+
+player_image = pygame.image.load("dragon_right.png")
+player_rect = player_image.get_rect()
+player_rect.left = 32
+player_rect.centery = WINDOW_HEIGHT //2
+
+
+
+
+
+pygame.mixer.music.play(-1, 0.0, 4)
+
+
+
+
 
 # The main game loop
 running = True
@@ -79,16 +99,16 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    # UPDATE HUD
-    score_text = Font.render("Score: " + str(score), True, GREEN, DARK_GREEN)
-    lives_text = Font.render("Lives: " + str(player_lives), True, GREEN, DARK_GREEN)
-
-    # Fill the display
+    #Fill the display
     display_surface.fill(BLACK)
 
-    # Blit the HUD to screen
     display_surface.blit(score_text, score_rect)
     display_surface.blit(title_text, title_rect)
     display_surface.blit(lives_text, lives_rect)
+    display_surface.blit(player_image, player_rect)
+
+    #Update display and tick the clock
+    pygame.display.update()
+    clock.tick(FPS)
 
 pygame.quit()
